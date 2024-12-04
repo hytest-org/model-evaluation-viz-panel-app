eaa51c4e8e921c4a7ea45f8bdfe590376c266361
lluddenusgs <pludden@contractor.usgs.gov>
2024-12-04 12:26:15 -0600

    Flipped files on accident

--- map.py
+++ map.py
@@ -1,102 +1,153 @@
-import geopandas as gpd
-import geoviews as gv
+import re
 import holoviews as hv
-import pandas as pd
+
+import geoviews as gv
 import panel as pn
-import intake
-from config import EX_STATES
-from map import Map
-from flow import FlowPlot
+import param
 from holoviews.streams import Selection1D
 
-pn.extension(notifications=True)
-hv.extension("bokeh")
+from config import STREAMGAGE_SUBSET
 
-# notifications
-pn.state.notifications.position = 'top-center'
-
-### PATHS  # noqa: E266
-states_path = "./data/geoBoundaries-USA-ADM1_simplified.geojson"
-streamgages_path = "./data/streamflow_gages_v1_n5390.csv"
-
-### DATA  # noqa: E266
-# read GeoJSON file
-def _get_state_data(_filepath: str) -> tuple[gpd.GeoDataFrame, list]:
-    '''
-    Read in state geographies from a specified file path.
-
-    Args:
-        _filepath (str): The file path to the geospatial data file containing state geometries.
-
-    Returns:
-        tuple[gpd.GeoDataFrame, list]: A tuple containing:
-            - A GeoDataFrame with the state geometries, excluding specified states.
-            - A sorted list of unique state names from the GeoDataFrame.
-    '''
-    # create 
-    _states = gpd.read_file(_filepath)
-    _states = _states[~_states['shapeName'].isin(EX_STATES)]
-
-    _states_list = list(_states['shapeName'].unique())
-    _states_list.sort()
-
-    return _states, _states_list
-
-states_data, states_list = _get_state_data(states_path)
-
-def _get_streamgage_data(_filepath: str) -> gpd.GeoDataFrame:
-    '''
-    Reads streamflow data from a .csv and filters it based on the 'gagesII_class==ref'.
-    Args:
-        _filepath (str): Path to the .csv file 
-    Returns:
-        gpd.GeoDataFrame: the filtered geopandas data file
-    '''
-    # read lat-long or xy data using pandas read_csv
-    read_data = pd.read_csv(_filepath, dtype=dict(site_no=str, nldi=int, swim=int, gfv1d1=int))
-
-    # filter
-    filtered_data = read_data[read_data['gagesII_class'] == 'Ref']
-    # now turn into a geodataframe
-    filtered_gdf = gpd.GeoDataFrame(filtered_data,  # noqa: W291
-                                    geometry=gpd.points_from_xy(filtered_data.dec_long_va, filtered_data.dec_lat_va),
-                                    crs="EPSG:4326"  # most data is exported in EPSG:4326
-    )
-    return filtered_gdf
-
-streamgage_data = _get_streamgage_data(streamgages_path)
-
-class class_Mediator:
-    def __init__(self, map_inst, flow_inst):
-        self.map = map_inst
-        self.flow = flow_inst
-        self.map.stream.param.watch(self.handle_tap, 'index')
-
-    def handle_tap(self, event):
-        if event.new: 
-            selected_index = event.new[0]
-            site_no = self.map.streamgages.iloc[selected_index]['site_no']
-            print(f"Selected {site_no}")
-            self.flow.set_site_id(site_no)
-
-
-
-
-map_inst = Map(states = states_data, streamgages = streamgage_data)
-flow_inst = FlowPlot()
-mediate = class_Mediator(map_inst,flow_inst)
-### WIDGET OPTIONS  # noqa: E266
-
-map_inst.param.state_select.objects = states_list
-model_eval = pn.template.MaterialTemplate(
-    title="HyTEST Model Evaluation",
-    sidebar=[
-        map_inst.param, flow_inst.param.start_date, flow_inst.param.end_date
-    ],
-    main=[map_inst.view, flow_inst.view],
+### Plot opts  # noqa: E266
+map_plot_opts = dict(
+    width=1200,
+    height=600,
+    title='United States Streamgage Map',
+    xaxis=None,
+    yaxis=None
 )
 
 
+class Map(param.Parameterized):
+    '''Instantiate map of CONUS.'''
+    states = param.DataFrame(precedence=-1)
+    streamgages = param.DataFrame(precedence=-1)
+    state_select = param.ListSelector(default=[], label="Select a State(s)", doc="Press the Ctrl button and left-click to select/deselect multiple states")
+    basemap_select = param.Selector(default="OSM", objects=gv.tile_sources.tile_sources.keys(), label="Select a Basemap")
+    streamgage_type_filter = param.Selector(objects=STREAMGAGE_SUBSET, default="all", label="Filter Streamgages by Type")
+    streamgage_id_input = param.String(doc='Enter a column delimited list e.g. 01022500, 01022502', label='Streamgage Site ID')
+    streamgage_id_string = param.String(precedence=-1, default="")
+    search_streamgage_id_input = param.Event(label="Search IDs")
+    clear_streamgage_id_input = param.Event(label="Clear IDs")
+    reset_map = param.Event(label="Reset Map")
+    stream = Selection1D(source=None)
 
+    def __init__(self, **params) -> None:
+        '''
+        Initialize the Map class with given parameters.
 
-model_eval.servable()
+        Args:
+            **params: Keyword arguments for parameter initialization.
+        '''
+        super().__init__(**params)
+        self.stream = Selection1D(source=None)
+        print("Stream assigned:", self.stream.source)
+
+    @param.depends("state_select")
+    def display_states(self) -> gv.Polygons:
+        '''
+        Display map of selected states.
+
+        Returns:
+            gv.Polygons: HoloViews Polygons object of the selected states.
+        '''
+        if self.state_select:
+            _states = gv.Polygons(self.states[self.states['shapeName'].isin(self.state_select)])
+        else:
+            _states = gv.Polygons(self.states)
+        return _states
+
+    @param.depends("basemap_select")
+    def display_basemap(self):
+        '''
+        Display the selected basemap.
+
+        Returns:
+            gv.TileSource: The selected basemap from tile sources.
+        '''
+        return gv.tile_sources.tile_sources[self.basemap_select]
+    
+    @param.depends("state_select", "streamgage_type_filter", "streamgage_id_string", watch=True)
+    def display_streamgages(self) -> gv.Points:
+        '''
+        Display streamgage points based on the selected filters.
+
+        Returns:
+            gv.Points: HoloViews Points object representing streamgages.
+        '''
+        # Give precedence to streamgage_id_input search
+        if len(self.streamgage_id_string) > 0:
+            id_list = [pid.strip() for pid in self.streamgage_id_string.split(",")]
+            streamgages_to_display = self.streamgages[self.streamgages['site_no'].isin(id_list)]
+        else:
+            column = self.streamgage_type_filter
+            if column != "all":
+                streamgages_to_display = self.streamgages[self.streamgages[column] == 1]
+            else:
+                streamgages_to_display = self.streamgages
+
+            if len(self.state_select) > 0:
+                streamgages_to_display = (streamgages_to_display.clip(self.states[self.states['shapeName'].isin(self.state_select)]))
+
+        site_gages = gv.Points(streamgages_to_display).opts(
+            tools=['hover','tap'],
+            cmap="Plasma",  # stand in and will be changed later
+            color="complete_yrs",  # stand in and will be changed later
+            size=5,
+            ) # stand in and will be changed later
+        self.stream.source = site_gages
+        print("strem source assigned:", self.stream.source)
+        return site_gages
+    
+    @param.depends("display_states", "display_basemap", "display_streamgages")
+    def view(self) -> pn.pane.HoloViews:
+        '''
+        Merge map components into a display.
+
+        Returns:
+            pn.pane.HoloViews: A Panel object containing the complete map view.
+        '''
+        return pn.pane.HoloViews(self.display_basemap() * self.display_states() * self.display_streamgages().options(**map_plot_opts))
+
+    # buttons
+    @param.depends("search_streamgage_id_input", watch=True)
+    def _update_streamgage_input(self)-> None:
+        '''
+        Updates the streamgage ID string based on the input provided. 
+        If the input contains invalid characters (anything other than 
+        digits, commas, or spaces), a warning notification is displayed.
+
+        Returns:
+            None
+        '''
+        if re.search('[^0-9, ]', self.streamgage_id_input):
+            pn.state.notifications.warning("Search included invalid characters. Please see the tooltip (?) for correct formatting.", duration=5000)
+        else:
+            self.streamgage_id_string = self.streamgage_id_input
+
+    @param.depends("clear_streamgage_id_input", watch=True)
+    def _clear_streamgage_input(self)-> None:
+        '''
+        Clears the streamgage ID input and string. 
+        This method sets both the input and string attributes to empty.
+
+        Returns:
+            None
+        '''
+        self.streamgage_id_string = ""
+        self.streamgage_id_input = ""
+
+    @param.depends("reset_map", watch=True)
+    def _reset_map(self) -> None:
+        '''
+        Resets all parameters to their default values except for a few specified parameters. 
+        This method iterates through all parameters and resets those that are not explicitly excluded.
+        
+        Returns:
+            None
+        '''
+        # loop through all params
+        for par in self.param:
+            # reset params with inputs
+            if par not in ["name", "streamgages", "states", "search_streamgage_id_input", "clear_streamgage_id_input", "reset_map"]:
+                setattr(self, par, self.param[par].default)
